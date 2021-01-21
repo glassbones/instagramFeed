65769f16d277aa1996939268ef72a3b9cb31a40f
Michael <glassbonesofficial@gmail.com>
2021-01-21 12:00:18 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import React, {useState, useEffect} from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
